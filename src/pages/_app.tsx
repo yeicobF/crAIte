3d7839f69291accddfb8c846c573fe8aea4ac92e
yeicobF <jacobrdz34@gmail.com>
2023-05-06 03:48:30 -0600

    Use optimized google font with next/font/google

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,22 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { Schibsted_Grotesk } from "next/font/google";
+
+// https://nextjs.org/docs/pages/building-your-application/optimizing/fonts#google-fonts
+// https://nextjs.org/docs/app/api-reference/components/font#subsets
+const schibstedGroteskFont = Schibsted_Grotesk({
+  display: "swap",
+  subsets: ["latin"],
+  preload: true,
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <div className={schibstedGroteskFont.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
 export default api.withTRPC(MyApp);
