a893115f44d8fa11875da8155febda77fd8ea7a8
yeicobF <jacobrdz34@gmail.com>
2023-05-06 07:30:12 -0600

    Add project info and logo variants

---
+++ src/constants/index.tsx
@@ -0,0 +1,21 @@
+const crIAte = {
+  name: "crIAte",
+  description: {
+    en: "Generate content for ads and posts for social networks using ChatGPT and DALLE 2 APIs.",
+    es: "Genera contenido para anuncios y posts para redes sociales usando las APIs de ChatGPT y DALLE 2.",
+  },
+  team: [
+    {
+      linkedIn: "",
+      github: "",
+      name: "",
+    },
+  ],
+};
+
+const LOGO_VARIANTS = {
+  light: "/logo/light.svg",
+  dark: "/logo/dark.svg",
+};
+
+export { crIAte, LOGO_VARIANTS };
