eb9d736fb5449a6b785c555cfef5618551146ad7
JAngelLopezR <angloru1@hotmail.com>
2023-05-07 03:36:16 -0600

    First form

--- src/pages/create/ad/index.tsx
+++ src/pages/create/ad/index.tsx
@@ -2,18 +2,11 @@ import { type NextPage } from "next";
 import { SectionLayout, SectionTitle } from "~/components/Section";
 import { useForm, SubmitHandler } from "react-hook-form";
 
-interface IFormInput {
-  firstName: string;
-  lastName: string;
-  age: number;
-}
-
 const CreateAd: NextPage = () => {
   
-  const { register, handleSubmit, watch, formState: { errors } } = useForm<IFormInput>();
-  const onSubmit: SubmitHandler<IFormInput> = data => console.log(data);
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<AdFields>();
+  const onSubmit: SubmitHandler<AdFields> = data => console.log(data);
 
-  console.log(watch("firstName")) // watch input value by passing the name of it
   return (
     <>
       <SectionLayout>
@@ -21,12 +14,82 @@ const CreateAd: NextPage = () => {
         <div>
           Here you will create a new Advertising
         </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input {...register("firstName", { required: true, maxLength: 20 })} />
-          <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
-          <input type="number" {...register("age", { min: 18, max: 99 })} />
-          <input type="submit" />
-        </form>
+        <div className="grid grid-cols-2 gap-4 w-full">
+            <form className="w-full flex-col gap-4" onSubmit={handleSubmit(onSubmit)}>
+              <div>
+                <div className="mb-2 block">
+                  <label htmlFor="about">What is my ad about?</label>
+                </div>
+                <input className="text-black" {...register("about", {required: true})} />
+              </div>
+              <div>
+                <div className="mb-2 block">
+                  <label htmlFor="type">What type of ad do I want to create?</label>
+                </div>
+                <input className="text-black" {...register("type", {required: true})} />
+              </div>
+              <div>
+                <div className="mb-2 block">
+                  <label htmlFor="name">What is the name of the product/service?</label>
+                </div>
+                <input className="text-black" {...register("name", {required: true})} />
+              </div>
+              <div>
+                <div className="mb-2 block">
+                  <label htmlFor="org">What is the name of my organization?</label>
+                </div>
+                <input className="text-black" {...register("org", {required: true})} />
+              </div>
+              {/* <div>
+                <div className="mb-2 block">
+                  <label htmlFor="keymsg">Could you say a single key message in one sentence?</label>
+                </div>
+                <input className="text-black" {...register("keymsg", {required: true})} />
+              </div> */}
+              <div>
+                <div className="mb-2 block">
+                  <label htmlFor="reasons">Why would people buy my product?</label>
+                </div>
+                <input className="text-black" {...register("reasons", {required: true})} />
+              </div>
+              <div>
+                <div className="mb-2 block">
+                  <label htmlFor="deadline">When does the ad stop being valid?</label>
+                </div>
+                <input className="text-black" {...register("deadline", {required: true})} />
+              </div>
+              <div>
+                <div className="mb-2 block">
+                  <label htmlFor="where">Where do I want to publish my ad?</label>
+                </div>
+                <input className="text-black" {...register("where", {required: true})} />
+              </div>
+              <div>
+                <div className="mb-2 block">
+                  <label htmlFor="tone">What do I want people to feel?</label>
+                </div>
+                <input className="text-black" {...register("tone", {required: true})} />
+              </div>
+              <div>
+                <div className="mb-2 block">
+                  <label htmlFor="who">Who would like to watch my ad?</label>
+                </div>
+                <input className="text-black" {...register("who", {required: true})} />
+              </div>
+              <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Generate Prompt
+              </button>
+            </form>
+            <div className="w-full flex-col gap-4">
+
+            </div>
+        </div>
+        {/* <form onSubmit={handleSubmit(onSubmit)}>
+          <input className="text-black" {...register("firstName", { required: true, maxLength: 20 })} />
+          <input className="text-black" {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+          <input className="text-black" type="number" {...register("age", { min: 18, max: 99 })} />
+          <input className="text-black" type="submit" />
+        </form> */}
       </SectionLayout>
     </>
   );
---
+++ src/types.ts
@@ -0,0 +1,12 @@
+type AdFields = {
+    about: string;
+    type: string;
+    name: string;
+    org: string;
+    // keymsg: string;
+    reasons: string;
+    deadline: string;
+    where: string;
+    tone: string;
+    who: string;
+}
