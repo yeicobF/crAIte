58a3853a12e81c129a0c22f2f9a8640af211ecef
yeicobF <jacobrdz34@gmail.com>
2023-05-06 21:47:58 -0600

    Update icons and favicon

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ type LogoVariant = keyof typeof LOGO_VARIANTS;
 
 export const Logo = (props: { size?: number; variant?: LogoVariant }) => {
   const imgSize = props.size ?? 32;
-  const variant = props.variant ?? LOGO_VARIANTS.light;
+  const variant = props.variant ?? LOGO_VARIANTS.dark;
   return (
     <Image
       src={variant}
